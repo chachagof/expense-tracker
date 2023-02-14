c45597ae3690c64a4eb6d921f15dc790884368e1
chachagof <chacha0932@gmail.com>
2023-02-14 15:33:44 +0800

    feat:add edit pafe

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ const port = 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// main page
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+// create page
 app.get('/expense/new',(req,res)=>{
   res.render('new')
+})
+
+// edit page
+app.get('/expense/edit',(req,res)=>{
+  res.render('edit')
 })
 
 app.listen(port, () => {
