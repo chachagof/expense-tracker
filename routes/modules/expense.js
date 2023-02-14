a0fe16ab1f2ef85e5f8896e158dbcada36550de2
chachagof <chacha0932@gmail.com>
2023-02-14 21:52:11 +0800

    feat:modify edit page

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -1,8 +1,7 @@
 const express = require('express')
+const expense = require('../../model/expense')
 const router = express.Router()
 const Expense = require('../../model/expense')
-
-
 
 // create page
 router.get('/new', (req, res) => {
@@ -22,8 +21,26 @@ router.post('/new', (req, res) => {
 })
 
 // edit page
-router.get('/edit', (req, res) => {
-  res.render('edit')
+router.get('/:_id/edit', (req, res) => {
+  const _id = req.params._id
+  Expense.findOne({_id})
+    .lean()
+    .then(expense =>{
+      res.render('edit',{expense})
+    })
+    .catch(err=>console.log(err))
+})
+router.post('/:_id/edit',(req,res)=>{
+  const _id = req.params._id
+  const { name, date, amount, category } = req.body
+  // checked name (undone)
+  if (name.trim() === '') {
+    return res.redirect('/expense/new')
+  }
+  const checkedName = name.trim()
+  Expense.findByIdAndUpdate(_id, { name:checkedName, date, amount, category})
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
