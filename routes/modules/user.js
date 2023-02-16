e6181aa63685e39018f74de00402def4798fefe7
chachagof <chacha0932@gmail.com>
2023-02-16 16:56:32 +0800

    feat:modify login & register route

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../model/user')
 
 // login
 router.get('/login',(req,res)=>{
@@ -7,7 +8,14 @@ router.get('/login',(req,res)=>{
 })
 
 router.post('/login',(req,res)=>{
-
+  const {email,password} = req.body
+  User.findOne({email})
+    .then(user=>{
+      if(!user)return res.redirect('/users/login')
+      if (user.password !== password) return res.redirect('/users/login')
+      return res.redirect('/')
+    })
+    .catch(err=>console.log(err))
 })
 // register
 router.get('/register',(req,res)=>{
@@ -15,7 +23,20 @@ router.get('/register',(req,res)=>{
 })
 
 router.post('/register',(req,res)=>{
-
+  const { name, email, password, confirmPassword } = req.body
+  if (password !== confirmPassword){
+    return res.redirect('/users/register')
+  }
+  User.findOne({email})
+    .then(user=>{
+      if(user){
+        return res.redirect('/users/register')
+      }
+      User.create({name,email,password})
+      return res.redirect('/users/login')
+    })
+    .catch(err=>console.log(err))
+  
 })
 
 // logout
