a381c413d04a98bb8c0ded6026f1340c3f503ead
chachagof <chacha0932@gmail.com>
2023-02-17 16:43:11 +0800

    feat: add sort in main page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
   const _id = req.user._id
   Expense.find({ userId: _id })
     .lean()
-    .sort({ name: "asc" })
+    .sort({ userId: 'asc' })
     .then(expense => {
       // add fontawesome
       expense.map(item => {
@@ -18,6 +18,28 @@ router.get('/', (req, res) => {
       })
       res.render('index', { expense })
     })
+})
+// sort page
+router.post('/sort', (req, res) => {
+  const { categoryId } = req.body
+  const _id = req.user._id
+  Expense.find({ userId: _id, category: categoryId })
+    .lean()
+    .sort({ userId: 'asc' })
+    .then(expense => {
+      if (expense.length === 0) {
+        req.flash('error_msg', 'Find nothings in this category')
+        return res.redirect('/')
+      }
+      // add fontawesome
+      expense.map(item => {
+        const index = item.category
+        item.icon = icon[index]
+      })
+      return res.render('index', { expense })
+    })
+    .catch(err => console.log(err))
+
 })
 
 module.exports = router
